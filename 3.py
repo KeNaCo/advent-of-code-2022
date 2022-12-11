f457392cd5d6206f5d242861a501feae3ec7cded
Ken Mijime <kenaco666@gmail.com>
2022-12-11 08:46:17 +0100

    feat(3): build item priority index

--- 3.py
+++ 3.py
@@ -1,4 +1,6 @@
 import unittest
+from itertools import chain
+from string import ascii_lowercase, ascii_uppercase
 
 
 def find_compartment_overlap(bag: str) -> set[str]:
@@ -20,3 +22,16 @@ class TestCompartmentOverlap(unittest.TestCase):
     def test_overlap_in_bag_with_overlap_compartments(self):
         self.assertEqual({"1"}, find_compartment_overlap(bag="121"))
         self.assertEqual({"1"}, find_compartment_overlap(bag="1231"))
+
+
+def build_item_priority_index() -> dict[str, int]:
+    index = {item: priority for item, priority in zip(chain(ascii_lowercase, ascii_uppercase), range(1, 53))}
+    return index
+
+
+class TestIndexBuild(unittest.TestCase):
+    def test_index_is_correct(self):
+        index = build_item_priority_index()
+        self.assertEqual(52, len(index))
+        self.assertEqual(1, index["a"])
+        self.assertEqual(52, index["Z"])
