9b374bd68a48e8f6e8db76729eef35fd097a653d
Ken Mijime <kenaco666@gmail.com>
2022-12-11 08:30:40 +0100

    feat(3): find overlap in non empty bag

--- 3.py
+++ 3.py
@@ -2,9 +2,21 @@ import unittest
 
 
 def find_compartment_overlap(bag: str) -> set[str]:
-    return set()
+    middle = len(bag) // 2
+    compartment1, compartment2 = set(bag[:middle]), set(bag[middle:])
+    return compartment1 & compartment2
 
 
 class TestCompartmentOverlap(unittest.TestCase):
     def test_overlap_of_an_empty_bag(self):
         self.assertEqual(set(), find_compartment_overlap(bag=""))
+
+    def test_overlap_in_bag_with_one_item(self):
+        self.assertEqual(set(), find_compartment_overlap(bag="1"))
+
+    def test_overlap_in_bag_with_more_items_without_overlap(self):
+        self.assertEqual(set(), find_compartment_overlap(bag="123"))
+
+    def test_overlap_in_bag_with_overlap_compartments(self):
+        self.assertEqual({"1"}, find_compartment_overlap(bag="121"))
+        self.assertEqual({"1"}, find_compartment_overlap(bag="1231"))
