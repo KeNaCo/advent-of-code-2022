3a364673dab151ac18622b284ba97f24e6032fff
Ken Mijime <kenaco666@gmail.com>
2022-12-11 10:33:59 +0100

    feat(3): scan bags for errors

--- 3.py
+++ 3.py
@@ -1,6 +1,7 @@
 import unittest
 from itertools import chain
 from string import ascii_lowercase, ascii_uppercase
+from typing import Sequence
 
 
 def find_compartment_overlap(bag: str) -> set[str]:
@@ -35,3 +36,21 @@ class TestIndexBuild(unittest.TestCase):
         self.assertEqual(52, len(index))
         self.assertEqual(1, index["a"])
         self.assertEqual(52, index["Z"])
+
+
+def scan_bags_for_errors(bags):
+    return chain.from_iterable(filter(lambda overlaps: overlaps, map(find_compartment_overlap, bags)))
+
+
+class TestScanBags(unittest.TestCase):
+    def test_no_bags(self):
+        self.assertEqual([], list(scan_bags_for_errors(bags=[])))
+
+    def test_one_bag_with_overlap(self):
+        self.assertEqual(["b"], list(scan_bags_for_errors(bags=["abcbd"])))
+
+    def test_one_bag_without_overlap_should_be_filtered_out(self):
+        self.assertEqual([], list(scan_bags_for_errors(bags=["abcdef"])))
+
+    def test_multiple_bags(self):
+        self.assertListEqual(["b", "a", "A"], list(scan_bags_for_errors(bags=["abcbef", "BaADEaFA", "ABCDEFG"])))
