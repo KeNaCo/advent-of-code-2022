10ff0ba81bc177566fd0173905d61e30abeea812
Ken Mijime <kenaco666@gmail.com>
2022-12-12 11:34:25 +0100

    feat(4): section range in operator for distinct ranges

---
+++ 4.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+import unittest
+
+
+@dataclass
+class SectionRange:
+    start: int  # included
+    stop: int  # included
+
+    def __contains__(self, item: "SectionRange") -> bool:
+        pass
+
+
+class TestSectionUseCases(unittest.TestCase):
+    def test_distinct_ranges_should_return_false(self):
+        section1 = SectionRange(1, 2)
+        section2 = SectionRange(3, 4)
+        result = section1 in section2
+        self.assertFalse(result)
