de0a56e6e38066e828ab84c137c53579506f2954
Ken Mijime <kenaco666@gmail.com>
2022-12-12 12:13:29 +0100

    feat(4): section range in operator for intersected ranges

--- 4.py
+++ 4.py
@@ -17,3 +17,9 @@ class TestSectionUseCases(unittest.TestCase):
         section2 = SectionRange(3, 4)
         result = section1 in section2
         self.assertFalse(result)
+
+    def test_intersecting_ranges_should_still_return_false(self):
+        section1 = SectionRange(1, 3)
+        section2 = SectionRange(2, 5)
+        result = section1 in section2
+        self.assertFalse(result)
