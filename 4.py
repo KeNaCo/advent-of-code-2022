ca3e3d802178a855b91c030c74ad069ebc776160
Ken Mijime <kenaco666@gmail.com>
2022-12-12 12:55:05 +0100

    feat(4): section range in operator for subset ranges

--- 4.py
+++ 4.py
@@ -8,7 +8,8 @@ class SectionRange:
     stop: int  # included
 
     def __contains__(self, item: "SectionRange") -> bool:
-        pass
+        is_in = (item.start >= self.start) and (item.stop <= self.stop)
+        return is_in
 
 
 class TestSectionUseCases(unittest.TestCase):
@@ -23,3 +24,27 @@ class TestSectionUseCases(unittest.TestCase):
         section2 = SectionRange(2, 5)
         result = section1 in section2
         self.assertFalse(result)
+
+    def test_one_range_inside_other_but_same_edge_should_return_true(self):
+        section1 = SectionRange(1, 2)
+        section2 = SectionRange(1, 3)
+        result = section1 in section2
+        self.assertTrue(result)
+
+    def test_one_range_inside_other_but_not_touching_the_edge_should_return_true(self):
+        section1 = SectionRange(3, 3)
+        section2 = SectionRange(1, 5)
+        result = section1 in section2
+        self.assertTrue(result)
+
+    def test_one_range_inside_other_but_operation_is_opposite_should_return_false(self):
+        section1 = SectionRange(1, 2)
+        section2 = SectionRange(1, 3)
+        result = section2 in section1
+        self.assertFalse(result)
+
+    def test_one_range_is_the_same_as_other_should_return_true(self):
+        section1 = SectionRange(1, 2)
+        section2 = SectionRange(1, 2)
+        result = section1 in section2
+        self.assertTrue(result)
